7a8aa517a76392cd7e6378a4f16b5e08ab57039d
Mikko Liikanen <mikko@quattrofolia.com>
2018-10-18 21:04:01 +0300

    Change Activity to use a ListView as the base view

--- app/src/main/java/mikkoliikanen/reposhower/RepoListingActivity.java
+++ app/src/main/java/mikkoliikanen/reposhower/RepoListingActivity.java
@@ -2,12 +2,16 @@ package mikkoliikanen.reposhower;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RepoListingActivity extends Activity {
+
+    private View repoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repo_listing);
+        repoList = findViewById(R.id.repo_list);
     }
 }
