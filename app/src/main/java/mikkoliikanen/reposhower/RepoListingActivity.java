988a5a23e5145d33e1fc04d2b348e652c6f07d47
Mikko Liikanen <mikko@quattrofolia.com>
2018-10-18 20:22:23 +0300

    Initial project setup

---
+++ app/src/main/java/mikkoliikanen/reposhower/RepoListingActivity.java
@@ -0,0 +1,13 @@
+package mikkoliikanen.reposhower;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RepoListingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repo_listing);
+    }
+}
